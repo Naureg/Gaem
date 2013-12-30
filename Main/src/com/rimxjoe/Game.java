719e59823953460bf8f26d4cc94d95ba7dc0ced5
joe <joe.lancas@gmail.com>
2013-12-30 11:20:00 +0000

    trying 2 werk

--- Main/src/com/rimxjoe/Game.java
+++ Main/src/com/rimxjoe/Game.java
@@ -1,15 +1,10 @@
 package com.rimxjoe;
 
-public class Game{
+import com.rimxjoe.screens.LoadingScreen;
 
-<<<<<<< HEAD
-=======
 public class Game extends com.badlogic.gdx.Game{
-
-    //this is a comment
     @Override
-    public void create() {
+    public void create(){
         setScreen(new LoadingScreen(this));
     }
->>>>>>> 551d98cc932b781e38044ff3f52ceeb5e992943d
 }
