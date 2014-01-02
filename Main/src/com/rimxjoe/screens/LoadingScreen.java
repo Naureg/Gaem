56f3a95dd136886ba141285956daed5c864b37ea
Rimdeker <rim@HAF>
2014-01-02 18:39:17 +0100

    Added a few assets Changed resolution Created GamePlayScreen with TiledMap renderer

--- Main/src/com/rimxjoe/screens/LoadingScreen.java
+++ Main/src/com/rimxjoe/screens/LoadingScreen.java
@@ -4,8 +4,17 @@ import com.rimxjoe.Game;
 
 public class LoadingScreen extends GameScreen {
 
+    //private short loaded = 0;
+
     public LoadingScreen(Game g) {
         super(g);
     }
+
+    @Override
+    public void show() {
+        g.setScreen(new GamePlayScreen());
+    }
+
+
 
 }
